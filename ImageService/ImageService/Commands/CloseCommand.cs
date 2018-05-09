5c7a4e3f072fd615dce30b4d5e5b1a913435f6de
thomasDayan <39137798+thomasDayan@users.noreply.github.com>
2018-05-09 13:39:03 -0700

    Add files via upload

---
+++ ImageService/ImageService/Commands/CloseCommand.cs
@@ -0,0 +1,45 @@
+﻿using ImageService.Commands;
+using ImageService.Controller.Handlers;
+using ImageService.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Commands
+{
+    /*
+     * 
+     * need to implemanet this shit 
+     * 
+     * 
+     * 
+     * 
+     * */
+
+
+    public class CloseCommand : ICommand
+    {
+        private IImageServiceModal model;
+        Dictionary<string, IDirectoryHandler> handlers;
+        public string Execute(string[] args, out bool result)
+        {
+            result = true;
+            try
+            {
+                if (args == null || args.Length == 0) { throw new Exception("invalid args");  }
+                IDirectoryHandler handler = handlers[args[1]];
+                string message = model.CloseHandler(handler, out result);
+            } catch (Exception e) { result = false; }
+
+            return "ok";
+        }
+
+        public CloseCommand(IImageServiceModal model , Dictionary<string, IDirectoryHandler> handlers)
+        {
+            this.model = model;
+            this.handlers = handlers;
+        }
+    }
+}
